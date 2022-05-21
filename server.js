5745fb8b5a5676e30ee657b7812c1bea473001ad
Radu Chirilov <radulin4a@gmail.com>
2022-05-21 20:23:20 +0300

    Connect server to MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+const { connectDB } = require('./config/db')
+const port = process.env.EXPRESS_PORT || 5000
+
+connectDB()
 
 const app = express();
-const mongoose = require('mongoose');
-
 app.use(cors());
 app.use(express.json());
 
 
-app.listen(process.env.EXPRESS_PORT, () => {
-    console.log(`Express app listening on port ${process.env.EXPRESS_PORT}`)
-});
+app.listen(port, () => console.log(`Express app listening on port ${port}`));
 
