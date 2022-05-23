6333c596d871e72a74e8c7376f55f480d5e54cde
Radu Chirilov <radulin4a@gmail.com>
2022-05-23 07:27:54 +0300

    Implement state management for Header

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,72 +1,91 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { login, reset } from "../features/auth/authSlice";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-  const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+    });
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const user = { ...formData };
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
 
-    //console.log(user); 
-    axios
-      .post("http://localhost:5000/api/v1.0/users/login", { ...user })
-      .then((res) => {
-        console.log(res);
-        navigate("/my-account")
-      })
-      .catch((err) => console.warn(err));
-  };
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
 
-  return (
-    <Container>
-      <br />
-      <br />
-      <Row className="d-flex justify-content-center">
-        <Col xs lg={4}>
-          <form onSubmit={handleSubmit}>
-            <h3>Login</h3>
-            <div className="mb-3">
-              <label>Username</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="username"
-                onChange={(e) =>
-                  setFormData({ ...formData, username: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="password"
-                onChange={(e) =>
-                  setFormData({ ...formData, password: e.target.value})
-                }
-              />
-            </div>
-            <div className="d-grid">
-              <button type="submit" className="btn btn-primary">
-                Login
-              </button>
-            </div>
-            <p className="forgot-password text-right">
-              Don't have an account? <Link to="/register">register</Link>
-            </p>
-          </form>
-        </Col>
-      </Row>
-    </Container>
-  );
+        if (isSuccess || user) {
+            navigate("/");
+        }
+
+        dispatch(reset());
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const user = { ...formData };
+        dispatch(login(user))
+    };
+
+    return (
+        <Container>
+            <br />
+            <br />
+            <Row className="d-flex justify-content-center">
+                <Col xs lg={4}>
+                    <form onSubmit={handleSubmit}>
+                        <h3>Login</h3>
+                        <div className="mb-3">
+                            <label>Username</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="username"
+                                onChange={(e) =>
+                                    setFormData({
+                                        ...formData,
+                                        username: e.target.value,
+                                    })
+                                }
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="password"
+                                onChange={(e) =>
+                                    setFormData({
+                                        ...formData,
+                                        password: e.target.value,
+                                    })
+                                }
+                            />
+                        </div>
+                        <div className="d-grid">
+                            <button type="submit" className="btn btn-primary">
+                                Login
+                            </button>
+                        </div>
+                        <p className="forgot-password text-right">
+                            Don't have an account?{" "}
+                            <Link to="/register">register</Link>
+                        </p>
+                    </form>
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default Login;
