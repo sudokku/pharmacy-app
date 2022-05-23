2d3e80ef2ac32527cdfc0e00e4793c11b993ced8
Tudor Toma <tudortoma2509@gmail.com>
2022-05-23 04:25:22 +0300

    Created Shop page and product page

--- client/src/views/Shop.js
+++ client/src/views/Shop.js
@@ -1,11 +1,34 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Shop = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() =>{
+    axios
+      .get('http://localhost:5000/api/v1.0/shop')
+      .then(res => {
+        setProducts(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return(
       <Container>
-          <h1>This is Shop</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatibus tempora distinctio? Eveniet a expedita reprehenderit debitis facilis dolore earum est sit voluptatum modi. Ratione dolorem tenetur itaque deserunt officia.</p>
+          <h1>Shop</h1>
+          <br></br>
+          <br></br>
+          <div>
+            <ul>
+              {
+                products.map(product => <li key={product._id}><Link to={product._id}>{product.name} {product.category} {product.price}</Link></li>)
+              }
+            </ul>
+          </div>
       </Container>
   );
 };
